331585d8ca8d870c1ab7a12b835ab5b118c79af4
Tomás Romera <tomas@flux1a.com.ar>
2025-08-13 14:32:52 +0000

    ...

--- src/app/proveedores/[name]/[code]/page.tsx
+++ src/app/proveedores/[name]/[code]/page.tsx
@@ -61,7 +61,7 @@ export default function ProductDetailPage() {
                         notFound();
                         return;
                     }
-
+ 
                     setProductInfo(productInfo);
                     setHistory(history);
 
