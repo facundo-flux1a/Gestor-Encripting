0654ae5b85455f8dbe1827c6c6ff52659ab0bf92
Tomás Romera <tomas@flux1a.com.ar>
2025-08-14 13:23:55 +0000

    quiero que mejores la vista de detalles del documento, que emisor, clien

--- src/app/documento/[id]/page.tsx
+++ src/app/documento/[id]/page.tsx
@@ -230,7 +230,7 @@ export default function DocumentoPage() {
                         </div>
                         </MainLayoutHeader>
                         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-                           <div className="lg:col-span-2">
+                           <div className="lg:col-span-2 space-y-8">
                              <DocumentView doc={doc} isEditing={isEditing} form={form} />
                            </div>
                            <div className="space-y-6">
