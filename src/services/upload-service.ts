b310a6ccceecc1e9aa87a709aa29df214b197882
facundo-flux1a <facundo@flux1a.com.ar>
2026-02-04 15:01:09 -0300

    fixsizes7

--- src/services/upload-service.ts
+++ src/services/upload-service.ts
@@ -1,7 +1,7 @@
 'use server';
 
 import { z } from 'zod';
-import { S3Client, PutObjectCommand, GetObjectCommand, CreateMultipartUploadCommand, UploadPartCommand, CompleteMultipartUploadCommand } from '@aws-sdk/client-s3';
+import { S3Client, PutObjectCommand, GetObjectCommand, CreateMultipartUploadCommand, UploadPartCommand, CompleteMultipartUploadCommand, ListObjectsCommand } from '@aws-sdk/client-s3';
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import crypto from 'crypto';
 import connection from '@/lib/db';
